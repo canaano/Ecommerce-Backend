0764db6d3ddd17c794efa58c0ccaee9b926dc88c
Canaan Owens <canaanowens@Canaans-MacBook-Pro-2021.local>
2023-01-09 16:26:23 -0800

    added routes for category routes and tested get

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,25 +16,71 @@ router.get("/", async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-  // find all categories
-  // be sure to include its associated Products
 });
 
-router.get("/:id", (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get("/:id", async (req, res) => {
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [
+        { model: Product, attributes: ["product_name", "price", "stock"] },
+      ],
+    });
+    if (!category) {
+      res.status(404).json({ message: "Category ID does not exist" });
+      return;
+    }
+
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post("/", (req, res) => {
-  // create a new category
+router.post("/", async (req, res) => {
+  try {
+    const category = await Category.create(req.body);
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put("/:id", (req, res) => {
-  // update a category by its `id` value
+router.put("/:id", async (req, res) => {
+  try {
+    const updateCategory = await Category.update(req.body, {
+      where: { id: req.params.id },
+    });
+
+    if (!updateCategory) {
+      res.status(404).json({
+        message: "Category ID does not exist",
+      });
+      return;
+    }
+    res.status(200).json(updateCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete("/:id", (req, res) => {
-  // delete a category by its `id` value
+// delete a category by its `id` value
+router.delete("/:id", async (req, res) => {
+  try {
+    const category = await Category.destroy({
+      where: { id: req.params.id },
+    });
+
+    if (!category) {
+      res.status(404).json({
+        message: "Category ID does not exist",
+      });
+      return;
+    }
+
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
