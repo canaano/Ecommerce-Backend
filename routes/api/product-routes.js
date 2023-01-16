c70e62bac33efa347338250e1671f7b512f3711a
Canaan Owens <canaanowens@Canaans-MacBook-Pro-2021.local>
2023-01-16 10:19:36 -0800

    updated Tag route logic

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -114,8 +114,26 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
-  // delete one product by its `id` value
+// delete one product by its `id` value
+router.delete("/:id", async (req, res) => {
+  try {
+    const product = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!product) {
+      res.status(404).json({
+        message: "Product ID does not exist",
+      });
+      return;
+    }
+
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
